b6e2775a597549cb45a942552b6e09c2facc672d
Dorian Leroy <dorian.leroy@inria.fr>
2022-04-15 17:24:11 +0200

    [doc] commented source files of simple-embedding

--- examples/simple-embedding/README.md
+++ examples/simple-embedding/README.md
@@ -1,5 +1,7 @@
 * Simple Embedding Example
 
+This example illustrates how to integrate monilog in your C++ application.
+
 In this directory:
  - Create and activate a Python venv.
  - Install pybind11: `pip install pybind11`
--- examples/simple-embedding/src/SimpleEmbedding.cc
+++ examples/simple-embedding/src/SimpleEmbedding.cc
@@ -7,27 +7,38 @@ namespace fs = std::filesystem;
 
 int main()
 {
+    // Retrieve the path where the Python scripts to use are located (can provide multiple locations).
     std::string path = fs::current_path();
     std::vector<std::string> python_path = { path + "/src/" };
+    // Provide the Python scripts to include.
     std::vector<std::string> python_scripts = {"example_moniloggers"};
+    // Name of the interface module (here, an embedded module declared above) exposing the execution context of the application.
     std::string interface_module = "example_interface";
+    // Initialization function for the interface module.
     std::function<void (pybind11::module_, pybind11::object)> interface_module_initializer =
             [](pybind11::module_ iterativeheatequation_module, pybind11::object context_class) { };
 
+    // Define base execution events emitted by the application, to which moniloggers can register.
     MoniLog::register_base_events({
         {"SomeEvent", 0},
         {"SomeOtherEvent", 1}
     });
     
+    // Define a composite event, emitted when either of its triggering events are emitted.
     MoniLog::register_composite_event("SomeCompositeEvent", {"SomeEvent", "SomeOtherEvent"});
 
+    // Instantiating the execution context accessible from Python.
     std::shared_ptr<MoniLog::MoniLogExecutionContext> ctx(new MoniLog::MoniLogExecutionContext());
 
+    // Bootstrapping monilog, consisting mainly of starting the Python interpreter, initializing
+    // the monilog module, and evaluating the provided scripts.
     MoniLog::bootstrap_monilog(python_path, python_scripts, interface_module, interface_module_initializer);
 
+    // Emitting some base events, triggering the registered moniloggers.
     MoniLog::trigger(0, ctx);
     MoniLog::trigger(1, ctx);
 
+    // Emitting a composite event (can also emit base event by name).
     MoniLog::trigger("SomeCompositeEvent", ctx);
 
 }
