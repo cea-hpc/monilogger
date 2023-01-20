dcb15ac3101cc3d85d1b63de31ffdee2fc86f98a
Dorian Leroy <dorian.leroy@cea.fr>
2023-01-20 17:24:38 +0100

    [dev] Add utility class DataFlowDict

--- src/api/scihook_module.cc
+++ src/api/scihook_module.cc
@@ -2,10 +2,12 @@
 
 PYBIND11_MODULE(_scihook, m) {
     m.attr("__name__") = "scihook._scihook";
-	py::class_<SciHook::SciHookExecutionContext, std::shared_ptr<SciHook::SciHookExecutionContext>>(m, "SciHookExecutionContext")
+
+    py::class_<SciHook::SciHookExecutionContext, std::shared_ptr<SciHook::SciHookExecutionContext>>(m, "SciHookExecutionContext")
         .def(py::init<>())
         .def(py::init(&SciHook::create_context))
         .def_property_readonly("name", &SciHook::SciHookExecutionContext::get_name);
+    
     m.def("register", &SciHook::register_scihook);
     m.def("stop", &SciHook::unregister_scihook);
     m.def("register_complex_event", &SciHook::register_complex_event);
@@ -19,4 +21,17 @@ PYBIND11_MODULE(_scihook, m) {
     {
         SciHook::trigger(event_id, scope);
     });
+
+    py::class_<SciHook::DataFlowDict>(m, "DataFlowDict")
+        .def("__bool__", [](const SciHook::DataFlowDict &self) -> bool { return !self.empty(); })
+        .def("__iter__", [](const SciHook::DataFlowDict &self) { return self.iter(); }, py::keep_alive<0, 1>())
+        .def("__getitem__", [](const SciHook::DataFlowDict &self, const py::object &key) -> std::vector<py::object> {
+                return self.get(key);
+            },
+            py::return_value_policy::reference_internal
+        )
+        .def("__contains__", [](const SciHook::DataFlowDict &self, const py::object &key) -> bool {
+            return self.contains(key);
+        });
+    
 }
